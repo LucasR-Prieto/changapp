982dbd9bef1aeb25a7b28522985d9c92f5d92550
doggbmx <manuelorticosta@gmail.com>
2022-05-02 20:52:43 -0800

    cambios manu

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask , render_template, url_for ,request , redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import true
@@ -9,78 +10,65 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/changapp.db'
 db  = SQLAlchemy(app)
 
-class usuario(db.Model):# pedir datos usuario
-    usuario_id = db.Column(db.Integer,primary_key=True)
-    usuario_nombre = db.Column(db.String(200))
-    usuario_apellido = db.Column(db.String(200))
-    usuario_desc = db.Column(db.String(200))
-    usuario_email = db.Column(db.String(200))
-    usuario_pass = db.Column(db.String(200))
-    usuario_done = db.Column(db.Boolean(200))
+class User(db.Model):# pedir datos usuario
+    user_id = db.Column(db.Integer,primary_key=True)
+    user_name = db.Column(db.String(200))
+    user_last_name = db.Column(db.String(200))
+    user_desc = db.Column(db.String(200))
+    user_email = db.Column(db.String(200))
+    user_pass = db.Column(db.String(200))
+    user_done = db.Column(db.Boolean(200))
 
 
-class empleo(db.Model): # datos empleo 
-    empleo_id = db.Column(db.Integer,primary_key=True)
-    empleo_nombre = db.Column(db.String(200))
-    empleo_desc = db.Column(db.String(200))
-    empleo_precio = db.Column(db.String(200))
-    empleo_done = db.Column(db.Boolean(200))
+class Job(db.Model): # datos empleo 
+    job_id = db.Column(db.Integer,primary_key=True)
+    job_name = db.Column(db.String(200))
+    job_desc = db.Column(db.String(200))
+    job_price = db.Column(db.String(200))
+    job_done = db.Column(db.Boolean(200))
 
 
 
 #configuracion inicio y datos para la base de datos
 
-@app.route("/")
-def loggin():
-    return render_template("loggin.html")
-
-
-@app.route("/registro")
-def inicio():
-    #Usuario = usuario.query.all()
-    return render_template("registro.html")#,Usuario=Usuario) # Usuario agrupa todas las tablas y muestra correcion del formulario html a la base de datos 
-
-
-
-@app.route("/empleo")
-def empleos():
-    Empleo = empleo.query.all()
-    return render_template("empleo.html", Empleo=Empleo)
-
-
-
-@app.route('/usuario', methods=['POST', 'GET']) # metodo post y get 
-def usuarios(): 
+@app.route("/login", methods=['POST'])
+def login():
     if request.method == 'POST':
-        Usuario = usuario(usuario_nombre=request.form['nombre'],
-                    usuario_apellido=request.form['apellido'],
-                    usuario_desc=request.form['descripcion'],
-                    usuario_email=request.form['email'],
-                    usuario_pass=request.form['password'],
-                    usuario_done = False
-                )
-        db.session.add(Usuario)
+        email = request.form['email']
+        password = request.form['password']
+    return redirect(url_for('index'))
+
+
+@app.route("/")
+def index():
+    return render_template("index.html") 
+
+@app.route('/register', methods=['POST'])
+def register(): 
+    if request.method == 'POST':
+        user = User(
+            user_name=request.form['name'],
+            user_last_name=request.form['last_name'],
+            user_desc=request.form['desc'],
+            user_email=request.form['email'],
+            user_pass=request.form['password'],
+            user_done = False
+        )
+        db.session.add(user)
         db.session.commit()
-        return redirect (url_for('usuarios'))
-    elif request.method == 'GET':
-        id = 1
-        Usuario= usuario.query.get(id)
-        print(Usuario)
-        return render_template('perfil.html',Usuario = Usuario)
+        return redirect(url_for('login'))
 
 
-
-
-@app.route('/nuevo_empleo', methods=['POST']) 
-def crear_empleo():
-    Empleo = empleo(nombre_empleo=request.form['empleo_nombre'],
-                descripcion=request.form['empleo_desc'],
-                precio=request.form['empleo_precio'],
-                done = False
+@app.route('/create-job', methods=['POST']) 
+def create_job():
+    job = Job(job_name=request.form['job_name'],
+                job_desc=request.form['job_desc'],
+                job_price=request.form['job_price'],
+                job_done = False
             )
-    db.session.add(Empleo)
+    db.session.add(job)
     db.session.commit()
-    return redirect (url_for('empleos'))
+    return redirect (url_for('index'))
 
 
 
