e67b34302a6cbcec711b9d61f8209601b5dcc22b
doggbmx <manuelorticosta@gmail.com>
2022-05-02 18:20:39 -0800

    cambios lucas trolo

--- app.py
+++ app.py
@@ -6,26 +6,25 @@ from sqlalchemy import true
 
 # congifuracion de la base de datos y de flask
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/tasks.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/changapp.db'
 db  = SQLAlchemy(app)
 
-class User(db.Model):# pedir datos empleado
-    id = db.Column(db.Integer,primary_key=True)
-    nombre = db.Column(db.String(200))
-    apellido = db.Column(db.String(200))
-    describe = db.Column(db.String(200))
-    contrasena = db.Column(db.String(200))
-    done = db.Column(db.Boolean(200))
+class usuario(db.Model):# pedir datos usuario
+    usuario_id = db.Column(db.Integer,primary_key=True)
+    usuario_nombre = db.Column(db.String(200))
+    usuario_apellido = db.Column(db.String(200))
+    usuario_desc = db.Column(db.String(200))
+    usuario_email = db.Column(db.String(200))
+    usuario_pass = db.Column(db.String(200))
+    usuario_done = db.Column(db.Boolean(200))
 
 
-class Empleador(db.Model): # datos Empleador 
-    id = db.Column(db.Integer,primary_key=True)
-    content = db.Column(db.String(200))
-    apellido = db.Column(db.String(200))
-    describe = db.Column(db.String(200))
-    tags = db.Column(db.String(200))
-    contrasena = db.Column(db.String(200))
-    done = db.Column(db.Boolean(200))
+class empleo(db.Model): # datos empleo 
+    empleo_id = db.Column(db.Integer,primary_key=True)
+    empleo_nombre = db.Column(db.String(200))
+    empleo_desc = db.Column(db.String(200))
+    empleo_precio = db.Column(db.String(200))
+    empleo_done = db.Column(db.Boolean(200))
 
 
 
@@ -36,53 +35,52 @@ def loggin():
     return render_template("loggin.html")
 
 
-@app.route("/ini")
+@app.route("/registro")
 def inicio():
-    tasks = User.query.all()
-    return render_template("ini.html",objeto1=tasks) # tasks agrupa tsodas las tareas y muestra 
-# corecion del formulario html a la base de datos 
+    #Usuario = usuario.query.all()
+    return render_template("registro.html")#,Usuario=Usuario) # Usuario agrupa todas las tablas y muestra correcion del formulario html a la base de datos 
 
 
 
-@app.route("/empleador")
-def inicio2():
-    empleos2 = Empleador.query.all()
-    return render_template("empleador.html", objeto2=empleos2)
+@app.route("/empleo")
+def empleos():
+    Empleo = empleo.query.all()
+    return render_template("empleo.html", Empleo=Empleo)
 
 
 
-@app.route('/user', methods=['POST', 'GET']) # metodo post es el metodo de conexion y create task es la ruta 
-def user(): # task es el nombre en el que guardamos la tarea
+@app.route('/usuario', methods=['POST', 'GET']) # metodo post y get 
+def usuarios(): 
     if request.method == 'POST':
-        user = User(nombre=request.form['nombre'],
-                    apellido=request.form['apellido'],
-                    describe=request.form['describe'],
-                    contrasena=request.form['contrasena'],
-                    done = False
+        Usuario = usuario(usuario_nombre=request.form['nombre'],
+                    usuario_apellido=request.form['apellido'],
+                    usuario_desc=request.form['descripcion'],
+                    usuario_email=request.form['email'],
+                    usuario_pass=request.form['password'],
+                    usuario_done = False
                 )
-        db.session.add(user)
+        db.session.add(Usuario)
         db.session.commit()
-        return redirect (url_for('inicio'))
+        return redirect (url_for('usuarios'))
     elif request.method == 'GET':
-        id = 4
-        user= User.query.get(id)
-        print(user)
-        return render_template('perfil.html',user = user)
+        id = 1
+        Usuario= usuario.query.get(id)
+        print(Usuario)
+        return render_template('perfil.html',Usuario = Usuario)
 
 
 
 
-@app.route('/create-empleador', methods=['POST']) # metodo post es el metodo de conexion y create task es la ruta 
-def crear_empleo(): # task es el nombre en el que guardamos la tarea
-    empleos2 = Empleador(content=request.form['content'],
-                apellido=request.form['apellido'],
-                describe=request.form['describe'],
-                contrasena=request.form['contrasena'],
+@app.route('/nuevo_empleo', methods=['POST']) 
+def crear_empleo():
+    Empleo = empleo(nombre_empleo=request.form['empleo_nombre'],
+                descripcion=request.form['empleo_desc'],
+                precio=request.form['empleo_precio'],
                 done = False
             )
-    db.session.add(empleos2)
+    db.session.add(Empleo)
     db.session.commit()
-    return redirect (url_for('inicio2'))
+    return redirect (url_for('empleos'))
 
 
 
