bb36ae04ec6932f8f9a69a1410244994d6194d96
doggbmx <manuelorticosta@gmail.com>
2022-05-03 19:38:50 -0800

    Login integrado y cambios varios

--- app.py
+++ app.py
@@ -2,15 +2,23 @@
 from flask import Flask, render_template, url_for, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import true
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, EmailField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 
 # congifuracion de la base de datos y de flask
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/changapp.db'
+app.config['SECRET_KEY'] = 'mysupersecretultrasecretkey1234'
 db  = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-class User(db.Model):# pedir datos usuario
-    user_id = db.Column(db.Integer,primary_key=True)
+class User(UserMixin, db.Model):# pedir datos usuario
+    id = db.Column(db.Integer,primary_key=True)
     user_name = db.Column(db.String(200))
     user_last_name = db.Column(db.String(200))
     user_desc = db.Column(db.String(200))
@@ -26,21 +34,38 @@ class Job(db.Model): # datos empleo
     job_price = db.Column(db.String(200))
     job_done = db.Column(db.Boolean(200))
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('email', validators=[InputRequired(), Length(min=4, max=25)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
 
 #configuracion inicio y datos para la base de datos
 @app.route("/", methods=['POST','GET'])
+@login_required
 def index():
-    return render_template("index.html")
+    return render_template("index.html", user=current_user)
 
 
 @app.route("/login", methods=['POST','GET'])
 def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        return redirect(url_for('index'))
-    else:
-        return render_template('login.html')
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(user_email=form.email.data).first()
+        if user:
+            print()
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('index'))
+
+        return '<h1>Invalid username or password</h1>'
+
+    return render_template('login.html', form=form)
 
 
 @app.route('/register', methods=['POST','GET'])
@@ -93,18 +118,11 @@ def profile():
     return render_template('user.html', user=users_profile)
 
 
-#eliminar datos 
-# @app.route("/delete/<id>")
-# def delete(id):
-#     task = User.query.filter_by(id=int(id)).delete()
-#     db.session.commit()
-#     return redirect (url_for('inicio'))
-
-# @app.route("/delete_empleo/<id>")
-# def delete_empleo(id):
-#     empleo2 = Empleador.query.filter_by(id=int(id)).delete()
-#     db.session.commit()
-#     return redirect (url_for('inicio2'))
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
